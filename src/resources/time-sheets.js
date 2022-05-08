8391c76bf8a924645afe6d03ce0a7c3c9289aeb9
meligiacone <melinabgiacone@gmail.com>
2022-05-08 19:48:07 -0300

    TG: add timesheet with validations

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -9,16 +9,23 @@ router.get('/', (req, res) => res.send(timesheets));
 
 // POST METHOD
 
-router.post('/addTimesheet', (req, res) => {
+router.post('/add', (req, res) => {
   const newTimesheet = req.body;
-  timesheets.push(newTimesheet);
-  fs.writeFile('src/data/time-sheets.json', JSON.stringify(timesheets), (error) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send('New timesheet created');
-    }
-  });
+  if (newTimesheet.id && newTimesheet.description && newTimesheet.day && newTimesheet.role
+    && newTimesheet.project && newTimesheet.task && newTimesheet.validated
+    && newTimesheet.projectId && newTimesheet.projectManager) {
+    timesheets.push(newTimesheet);
+    fs.writeFile('src/data/time-sheets.json', JSON.stringify(timesheets), (error) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send('New timesheet created');
+      }
+    });
+    res.send('New timesheet created');
+  } else {
+    res.send('need more data');
+  }
 });
 
 // PUT METHOD
