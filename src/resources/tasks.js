c87ce4cea0b5045d3ddb76548425642cf5c58710
RaquelFerrari <raqueferrari@gmail.com>
2022-05-10 21:11:41 -0300

    TG-03: show tasks & obtain task

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -9,7 +9,7 @@ router.get('/getAll', (req, res) => {
   res.send(tasks);
 });
 
-// Obtain a task
+// Obtain a task by ID
 router.get('/taskId/:taskID', (req, res) => {
   const findTask = req.params.taskID;
   const taskFound = tasks.find((task) => task.taskID === findTask);
@@ -17,6 +17,17 @@ router.get('/taskId/:taskID', (req, res) => {
     res.send(taskFound);
   } else {
     res.send('Task not found');
+  }
+});
+
+// Obtain a task by Name
+router.get('/taskName/:taskName', (req, res) => {
+  const nameTask = req.params.taskName;
+  const taskNamed = tasks.filter((task) => task.taskName === nameTask);
+  if (taskNamed.length > 0) {
+    res.send(taskNamed);
+  } else {
+    res.send(`"${nameTask}" is not a defined task`);
   }
 });
 
