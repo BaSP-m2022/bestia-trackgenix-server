5e5fade1df2a655fc930332b5fded53bc1fdf89f
RaquelFerrari <raqueferrari@gmail.com>
2022-05-10 21:06:36 -0300

    TG-03: show tasks & obtain task

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -4,9 +4,20 @@ const tasks = require('../data/tasks.json');
 
 const router = express.Router();
 
-// Obtain Tasks
+// Show Tasks
 router.get('/getAll', (req, res) => {
   res.send(tasks);
+});
+
+// Obtain a task
+router.get('/taskId/:taskID', (req, res) => {
+  const findTask = req.params.taskID;
+  const taskFound = tasks.find((task) => task.taskID === findTask);
+  if (taskFound) {
+    res.send(taskFound);
+  } else {
+    res.send('Task not found');
+  }
 });
 
 module.exports = router;
