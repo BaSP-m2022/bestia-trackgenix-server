58aa57168a108068e387b81b66face9468d561e7
martinlotufo93 <101262011+martinlotufo93@users.noreply.github.com>
2022-05-19 23:03:06 -0300

    TG-6: Merge Changes

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ mongoose.connect(URI)
   .catch((error) => console.error(error));
 
 app.listen(port, () => {
+   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
 
